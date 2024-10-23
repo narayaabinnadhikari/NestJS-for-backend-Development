771841a4ab9f134c896cfe9c327c31c254e4aa22
naraabinnadhikari <narayanabin223@gmail.com>
2024-10-23 12:49:32 +0545

    3. Providers and api testing

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,17 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
+
 
 @Controller('users') //  /users
 export class UsersController {
 
+    constructor(private readonly usersService: UsersService)  {
+
+    } 
+
     @Get()  //  GET/users or /users?role=value&age=value etc
     findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' |'ADMIN') {
-        return []
+        return this.usersService.findAll(role)
     }
 
     // Order is Important, it is like waterfall.
@@ -16,23 +22,23 @@ export class UsersController {
 
     @Get(':id')  // GET /users/:id
     findOne(@Param('id') id: string) {
-        return { id }
+        return this.usersService.findOne(+id)
     }
 
     @Post()  //POST /users
-    create(@Body() user: {}) {
-        return user
+    create(@Body() user: {name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN' }) {
+        return this.usersService.create(user)
     }
 
     @Patch(':id')  // GET /users/:id
-    update(@Param('id') id: string, @Body() userUpdate: {}) {
-        return { id, ...userUpdate }
+    update(@Param('id') id: string, @Body() userUpdate: {name?: string,  email?: string, role?: 'INTERN' | 'ENGINEER' | 'ADMIN' }) {
+        return this.usersService.update(+id, userUpdate)
     }
 
     
     @Delete(':id')  //   DELETE /users/:id
     delete(@Param('id') id: string) {
-        return { id }
+        return this.usersService.delete(+id)
     }
     
 }
