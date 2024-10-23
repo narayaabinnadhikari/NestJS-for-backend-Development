92c0443d9b30d9a7d71c7aaaaace0bd20bd52b97
naraabinnadhikari <narayanabin223@gmail.com>
2024-10-23 11:58:59 +0545

    @Controller('user') created.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+@Controller('users') //  /users
+export class UsersController {
+
+    @Get()  //  GET/users or /users?role=value&age=value etc
+    findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' |'ADMIN') {
+        return []
+    }
+
+    // Order is Important, it is like waterfall.
+    // @Get('interns')  //  GET/users/interns
+    // findAllInterns() {
+    //     return []
+    // }
+
+    @Get(':id')  // GET /users/:id
+    findOne(@Param('id') id: string) {
+        return { id }
+    }
+
+    @Post()  //POST /users
+    create(@Body() user: {}) {
+        return user
+    }
+
+    @Patch(':id')  // GET /users/:id
+    update(@Param('id') id: string, @Body() userUpdate: {}) {
+        return { id, ...userUpdate }
+    }
+
+    
+    @Delete(':id')  //   DELETE /users/:id
+    delete(@Param('id') id: string) {
+        return { id }
+    }
+    
+}
